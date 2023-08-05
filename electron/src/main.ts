72b567d69d92d9facee6204adf7bb9d98cff88b0
k-isaka <isk.rugby@gmail.com>
2023-08-05 22:02:49 +0900

    feat: hotReload機能の追加

--- electron/src/main.ts
+++ electron/src/main.ts
@@ -1,17 +1,30 @@
 import { app, BrowserWindow } from 'electron'
+import isDev = require('electron-is-dev')
 import * as path from 'path'
 
 const mainURL = `file:${__dirname}/../../index.html`
 
 const createWidnow = () => {
-    const mainWindow = new BrowserWindow({
+    let mainWindow = new BrowserWindow({
         width: 500,
         height: 500,
         webPreferences: {
             preload: path.join(__dirname, "preload.js")
         }
     })
-    mainWindow.loadURL(mainURL)
+    if (isDev) {
+        mainWindow.loadURL("http://localhost:3000/index.html")
+    } else {
+        mainWindow.loadURL(mainURL)
+    }
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+    if (isDev) {
+        require('electron-reload')(__dirname, {
+            electron: require(`${__dirname}/../node_modules/electron`)
+        });
+    }
 }
 
 app.whenReady().then(() => {
