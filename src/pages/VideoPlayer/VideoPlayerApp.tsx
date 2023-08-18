7649b6be9b28059c9a064636f4ce7cb9408184cc
k-isaka <isk.rugby@gmail.com>
2023-08-18 15:44:09 +0900

    feat: 複数の動画をコントロールできるように実装(WIP)

--- src/pages/VideoPlayer/VideoPlayerApp.tsx
+++ src/pages/VideoPlayer/VideoPlayerApp.tsx
@@ -1,5 +1,8 @@
+import React, { useState, useEffect, useRef } from "react";
+import videojs from "video.js";
+import { Box, Button } from "@mui/material";
 import { VideoPlayerView } from "./VideoPlayerView";
-import { Box } from "@mui/material";
+import Player from "video.js/dist/types/player";
 
 export const VideoPlayerApp = () => {
     const videoList = [
@@ -7,13 +10,36 @@ export const VideoPlayerApp = () => {
         '/Users/isakakou/Desktop/MAH00122.MP4'
     ];
 
+    const [players, setPlayers] = useState<Player[]>([]);
+    const videoRefs = useRef<Player[]>([]);
+
+    useEffect(() => {
+        videoRefs.current = videoList.map(() => {
+            const player = videojs(document.createElement('video'));
+            return player;
+        });
+
+        setPlayers(videoRefs.current);
+
+        return () => {
+            videoRefs.current.forEach(player => player.dispose());
+        };
+    }, [videoList]);
+
+    const handleJump = (time: number) => {
+        players.forEach(player => {
+            player.currentTime(time);
+        });
+    };
+
     return (
         <>
             <Box sx={{ display: 'flex', flexDirection: 'row' }}>
                 {videoList.map((filePath, index) => (
-                    <VideoPlayerView id={'video' + index.toString()} filePath={filePath} />
+                    <VideoPlayerView key={index} id={'video' + index.toString()} filePath={filePath} player={players[index]} />
                 ))}
             </Box>
+            <Button onClick={() => handleJump(10)}>Jump to 10 seconds</Button>
         </>
     );
 };
--- src/pages/VideoPlayer/VideoPlayerView.tsx
+++ src/pages/VideoPlayer/VideoPlayerView.tsx
@@ -1,8 +1,32 @@
-export const VideoPlayerView = ({ id, filePath }: { id: string, filePath: string }) => {
+import React, { useEffect, useRef } from "react";
+import videojs from "video.js";
+import Player from "video.js/dist/types/player";
+
+type VideoPlayerViewProps = {
+    id: string;
+    filePath: string;
+    player: Player | null;
+};
+
+export const VideoPlayerView: React.FC<VideoPlayerViewProps> = ({ id, filePath, player }) => {
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    useEffect(() => {
+        if (videoRef.current && player) {
+            player.src(filePath);
+            player.load();
+            player.on("timeupdate", () => {
+                const currentTime = player.currentTime();
+                player.tech_.trigger('play');
+                player.tech_.setCurrentTime(currentTime);
+            });
+        }
+    }, [filePath, player]);
 
     return (
         <div>
             <video
+                ref={videoRef}
                 id={id}
                 className="video-js"
                 controls
@@ -10,7 +34,6 @@ export const VideoPlayerView = ({ id, filePath }: { id: string, filePath: string
                 width="800"
                 height="450"
                 data-setup="{}"
-                autoPlay={true}
             >
                 <source src={filePath} type="video/mp4" />
             </video>
