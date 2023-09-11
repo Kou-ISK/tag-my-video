d1e84a84c56a94cbb75d191754add4a01aed45f8
k-isaka <isk.rugby@gmail.com>
2023-09-11 22:02:18 +0900

    feat: 不要なファイルを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import './App.css';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import { MainView } from './pages/MainView';
-import { TodoApp } from './pages/TodoApp';
-import { Header } from './components/Header';
 import { VideoPlayerApp } from './pages/VideoPlayerApp';
 
 function App() {
   return (
     <>
-      <HashRouter>
-        <Header />
-        <Routes>
-          <Route path='/' element={<MainView />} />
-          <Route path='/todo' element={<TodoApp />} />
-          <Route path='/video' element={<VideoPlayerApp />} />
-        </Routes>
-      </HashRouter>
+      <VideoPlayerApp />
     </>
   );
 }
